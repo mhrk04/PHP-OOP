416bf933c24da031791a741ed50a5822c52dd1a1
mhrk04 <haziqrohaizan@gmail.com>
2022-04-23 10:03:36 +0800

    manipulate properties

---
+++ properties.php
@@ -0,0 +1,13 @@
+<?php
+require_once "./data/Person.php"; // class telah dibuat di file ini
+// person === object
+$person = new Person();
+
+$person->name = "Haziq";
+$person->address = "Puchong";
+$person->country = "Malaysia";
+
+
+echo "Nama : $person->name" . PHP_EOL;
+echo "Nama : $person->address" . PHP_EOL;
+echo "Nama : $person->country" . PHP_EOL;
