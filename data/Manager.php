0f3d16c65697f176e8b5a4dbc40c446fd7f40c33
mhrk04 <haziqrohaizan@gmail.com>
2022-04-24 18:21:31 +0800

    Constructor Overriding

--- data/Manager.php
+++ data/Manager.php
@@ -2,7 +2,13 @@
 class Manager
 {
   var string $name;
+  var string $title;
   // :void == tiada return value
+  public function __construct(string $name = "", string $title = "Manager")
+  {
+    $this->name = $name;
+    $this->title = $title;
+  }
   function sayHello(string $name): void
   {
     echo "Hi $name, my name is Manager $this->name" . PHP_EOL;
@@ -10,7 +16,13 @@ class Manager
 }
 // Inheritance
 class VicePres extends Manager
-{ // :void == tiada return value
+{
+  public function __construct(string $name = "")
+  {
+    parent::__construct($name, "VP");
+  }
+
+  // :void == tiada return value
   function sayHello(string $name): void
   {
     echo "Hi $name, my name is VP $this->name" . PHP_EOL;
